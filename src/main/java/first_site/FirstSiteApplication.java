380e09034fbe670ccfb7a4691496b3a22ada6aaa
ansamots <33481586+ansamots@users.noreply.github.com>
2020-10-23 18:17:33 +0300

    Создание из шаблона

---
+++ src/main/java/first_site/FirstSiteApplication.java
@@ -0,0 +1,13 @@
+package first_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSiteApplication.class, args);
+	}
+
+}
