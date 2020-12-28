3a4227b1171e4063df11ebc469d71b44e79b9ee4
ansamots <33481586+ansamots@users.noreply.github.com>
2020-12-28 12:13:14 +0300

    Первые наброски сайта.

---
+++ src/main/java/first_site/controllers/Test.java
@@ -0,0 +1,12 @@
+package first_site.controllers;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+ class Test {
+    public static void main(String[] args){
+        byte[] b = new byte[256];
+        System.out.println("Hello");
+    }
+
+}
